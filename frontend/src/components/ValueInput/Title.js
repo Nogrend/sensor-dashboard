838e9cbb7520a2c2698b9273955211f2b747c4b9
BOUB <bram.bouwens@witteveenbos.com>
2021-12-09 20:59:18 +0100

    add experiment component

---
+++ frontend/src/components/ValueInput/Title.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+function Title({ value }) {
+  return <h1>{value}</h1>;
+}
+export default Title;
---
+++ frontend/src/components/ValueInput/ValueInput.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import NumberFormat from "react-number-format";
+import Title from "./Title";
+import "./ValueInput.css";
+
+const LANGUAGE = {
+  EN: "EN",
+  NL: "NL",
+};
+
+function ValueInput() {
+  const [language, setLanguage] = useState("EN");
+  const [value, setValue] = useState("");
+
+  function buttonHandler() {
+    setLanguage(LANGUAGE.EN === language ? LANGUAGE.NL : LANGUAGE.EN);
+    // setValue(document.getElementById("number").value);
+  }
+  function submitHandler() {
+    let number = 0;
+    if (isEnglish) {
+      number = Number(value.replace(/,/g, ""));
+    } else {
+      number = Number(value.replace(/\./g, "").replace(",", "."));
+    }
+    console.log(number);
+  }
+
+  const inputHandler = (e) => {
+    setValue(e.target.value);
+  };
+  const isEnglish = LANGUAGE.NL === language;
+
+  return (
+    <div>
+      <Title value={value} />
+      <div>
+        <label>{isEnglish ? LANGUAGE.EN : LANGUAGE.NL} </label>
+        <NumberFormat
+          id="number"
+          className="Input"
+          thousandSeparator={isEnglish ? "," : "."}
+          decimalSeparator={isEnglish ? "." : ","}
+          onChange={inputHandler}
+        />
+      </div>
+      <div className="ValueInput">
+        <button className="Button" onClick={buttonHandler}>
+          {language}
+        </button>
+        <button className="Button" onClick={submitHandler}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
+}
+export default ValueInput;
